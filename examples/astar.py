f215750683be641840fd115811322b9029058913
puru07 <pururastogi@gmail.com>
2017-03-26 19:48:29 -0400

    modeified astar to djikstra and inserted the node class into it

--- examples/astar.py
+++ examples/astar.py
@@ -5,22 +5,63 @@ except:
 from Node import node
 import Node 
 
+class node:
 
+	# dimensions of the arena
+	arena_W = 7		# along x
+	arena_L = 7
+
+	def __init__(self,x,y,gcost,parentkey,childnum):
+		self.x = x
+		self.y = y
+		self.parentkey = parentkey 
+		self.childnum = childnum +1
+		self.gcost = gcost + r_map[x][y]
+
+	def getSuccs(self, r_map):
+		x = self.x
+		y = self.y
+		# list of successors to be returned
+		succs = [] 
+		if x + 1 < arena_W +1:
+			succs += node(x+1,y self.gcost,self.gethashKey(),self.childnum)
+		if x-1 >-1 :
+			succs += node(x-1,y self.gcost,self.gethashKey(),self.childnum)
+		if y + 1 < arena_L +1 :
+			succs += node(x,y+1 self.gcost,self.gethashKey(),self.childnum)
+		if y-1 > -1:
+			succs += node(x,y-1 self.gcost,self.gethashKey(),self.childnum)
+		return succs
+	def gethashKey(self):
+		haskey = self.x*100 + self.y
+		return int(haskey)
+
+
+def getCost(Node, Weight):
+	return (Weight*Node.getHcost() + Node.gcost)
+
+def getSatefromKey(key):
+	x = key//100
+	y = (key-x*100)
+
+	return [x,y]
 def init(arena):
 
 	node.arena_W = arena[0]
 	node.arena_L = arena[1]
 
-def astar(start,goal,weight, r_map):
+def astar(start,r_map, traj_lim):
 	#print 	node.door_r ,node.arm_r , node.Width , node.Len ,node.arena_W , node.arena_L 
 	closelist = {}
 	openlist = pq()
 	openlist_f = {}
 	gstart = 0
 	parent_start = 0
-	start_node = node(start[0],start[1],gstart,parent_start)
+	childnum = -1
+	start_node = node(start[0],start[1],gstart,parent_start,childnum)
 	openlist.put((Node.getCost(start_node,weight),start_node))
 	openlist_f[start_node.gethashKey()] = start_node
+
 	while (openlist.qsize()>0):
 		# print 'size of open list'
 		# print openlist.qsize()
@@ -33,7 +74,7 @@ def astar(start,goal,weight, r_map):
 		#print 'the node expand'
 		#printNode(node2exp)
 		#checking if the node2exp is the goal
-		if node2exp.getHcost() == 0.00 :
+		if node2exp.childnum == traj_lim :
 			print 'goal reached via hcost'
 			Node.printNode(node2exp)
 			print node2exp.getFeasibleAngle()
@@ -41,10 +82,6 @@ def astar(start,goal,weight, r_map):
 			print len(closelist)
 			print 'teh cost'
 			print Node.getCost(node2exp, weight)	
-			return [node2exp,closelist]
-		if node2exp.y >0 :
-			print 'goal reached'
-			Node.printNode(node2exp)
 			return [node2exp,closelist]
 
 		#expanding the node
@@ -59,7 +96,7 @@ def astar(start,goal,weight, r_map):
 				# print 'duplicate in open list'
 				continue
 			# printNode(item)
-			openlist.put((Node.getCost(item,weight),item))
+			openlist.put((item.gcost,item))
 			openlist_f[item.gethashKey()] = item
 
 		# moving the expanded node to close list, and removing from open list dict
